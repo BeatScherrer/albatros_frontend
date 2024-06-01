b59a859bdce1fb9ad70fd5a76bcd44efa9cd1a13
Beat Scherrer <beat.scherrer@gmail.com>
2024-06-01 22:59:01 +0200

    format

--- src/notifications.rs
+++ src/notifications.rs
@@ -1,6 +1,6 @@
+use iced::widget::{button, column, container, horizontal_rule, horizontal_space, row, text};
+use iced::{advanced::Widget, Element, Length, Renderer, Size, Theme};
 use iced::{Alignment, Event};
-use iced::{advanced::Widget, Theme, Renderer, Size, Length, Element};
-use iced::widget::{button, column, row, horizontal_rule, container, text, horizontal_space};
 
 #[allow(dead_code, unused_imports)]
 
@@ -33,8 +33,8 @@ use iced::widget::{button, column, row, horizontal_rule, container, text, horizo
 pub const DEFAULT_TIMEOUT: u64 = 5;
 
 pub enum Message {
-  Event(Event),
-  Dummy
+    Event(Event),
+    Dummy,
 }
 
 #[derive(Debug, Clone, Default)]
@@ -49,53 +49,50 @@ pub struct Notifications<'a, Message> {
     timeout_secs: u64,
 }
 
-impl <'a, Message> Notifications<'a, Message>
-where Message: 'a + Clone,
+impl<'a, Message> Notifications<'a, Message>
+where
+    Message: 'a + Clone,
 {
-pub fn new(content: impl Into<Element<'a, Message>>, notifications: &'a [Notification], on_close: impl Fn(usize) -> Message + 'a) -> Self {
-            let notifications = notifications
-                .iter()
-                .enumerate()
-                .map(|(index, notification)| {
-                    container(column![
-                        container(
-                            row![
-                                text(notification.title.as_str()),
-                                horizontal_space(),
-                                button("X")
-                                    .on_press((on_close)(index))
-                                    .padding(3),
-                            ]
-                            .align_items(Alignment::Center)
-                        )
+    pub fn new(
+        content: impl Into<Element<'a, Message>>,
+        notifications: &'a [Notification],
+        on_close: impl Fn(usize) -> Message + 'a,
+    ) -> Self {
+        let notifications = notifications
+            .iter()
+            .enumerate()
+            .map(|(index, notification)| {
+                container(column![
+                    container(
+                        row![
+                            text(notification.title.as_str()),
+                            horizontal_space(),
+                            button("X").on_press((on_close)(index)).padding(3),
+                        ]
+                        .align_items(Alignment::Center)
+                    )
+                    .width(Length::Fill)
+                    .padding(5),
+                    horizontal_rule(1),
+                    container(text(notification.message.as_str()))
                         .width(Length::Fill)
-                        .padding(5),
-                        // .style(match toast.status {
-                        //     Status::Primary => primary,
-                        //     Status::Secondary => secondary,
-                        //     Status::Success => success,
-                        //     Status::Danger => danger,
-                        // }),
-                        horizontal_rule(1),
-                        container(text(notification.message.as_str()))
-                            .width(Length::Fill)
-                            .padding(5)
-                            .style(container::rounded_box),
-                    ])
-                    .max_width(200)
-                    .into()
-                })
-                .collect();
+                        .padding(5)
+                        .style(container::rounded_box),
+                ])
+                .max_width(200)
+                .into()
+            })
+            .collect();
 
-                Self {
-                content: content.into(),
-                notifications,
-                timeout_secs: DEFAULT_TIMEOUT,
-                }
-           }
+        Self {
+            content: content.into(),
+            notifications,
+            timeout_secs: DEFAULT_TIMEOUT,
+        }
+    }
 }
 
-impl <'a, Message> Widget<Message, Theme, Renderer> for Notifications<'a, Message> {
+impl<'a, Message> Widget<Message, Theme, Renderer> for Notifications<'a, Message> {
     fn size(&self) -> Size<Length> {
         self.content.as_widget().size()
     }
@@ -106,7 +103,9 @@ impl <'a, Message> Widget<Message, Theme, Renderer> for Notifications<'a, Messag
         renderer: &Renderer,
         limits: &iced::advanced::layout::Limits,
     ) -> iced::advanced::layout::Node {
-        self.content.as_widget().layout(&mut tree.children[0], renderer, limits)
+        self.content
+            .as_widget()
+            .layout(&mut tree.children[0], renderer, limits)
     }
 
     fn draw(
@@ -119,15 +118,23 @@ impl <'a, Message> Widget<Message, Theme, Renderer> for Notifications<'a, Messag
         cursor: iced::advanced::mouse::Cursor,
         viewport: &iced::Rectangle,
     ) {
-        self.content.as_widget().draw(&tree.children[0], renderer, theme, style, layout, cursor, viewport);
+        self.content.as_widget().draw(
+            &tree.children[0],
+            renderer,
+            theme,
+            style,
+            layout,
+            cursor,
+            viewport,
+        );
     }
 }
 
-
 impl<'a, Message> From<Notifications<'a, Message>> for Element<'a, Message>
-where Message: 'a
-      {
-        fn from(value: Notifications<'a, Message>) -> Self {
-            Element::new(value)
+where
+    Message: 'a,
+{
+    fn from(value: Notifications<'a, Message>) -> Self {
+        Element::new(value)
     }
-    }
+}
